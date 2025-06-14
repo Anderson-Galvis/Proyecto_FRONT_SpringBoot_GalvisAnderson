153ebb989f263dc4b7ebc9e3647c5171cacad1f9
Anderson-Galvis <stiven.g2031@gmail.com>
2025-06-14 10:59:29 -0500

    Se inicializa el proyecto en react agregando tambien el login

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LoginForm from './component/LoginForm.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
